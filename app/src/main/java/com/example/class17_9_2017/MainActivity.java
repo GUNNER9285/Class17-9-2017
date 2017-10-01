322e8fb9d0eeba41adbc669911e14738093b1fc8
GUNNER9285 <gunner9285@gmail.com>
2017-10-01 09:15:02 +0700

    Lern OnClickListener

--- app/src/main/java/com/example/class17_9_2017/MainActivity.java
+++ app/src/main/java/com/example/class17_9_2017/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.class17_9_2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button okButton = (Button)findViewById(R.id.okButton);
+        EditText emailEditText = (EditText)findViewById(R.id.emailEditText);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast bread = Toast.makeText(MainActivity.this, "OK หาพ่อง", Toast.LENGTH_SHORT);
+                bread.show();
+            }
+        });
+        Button exitButton = (Button) findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
 }
